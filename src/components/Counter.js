ffc6c2835f2a7f1b95a3f5785e51d2cfdf36cfcb
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-03-19 14:03:20 +0530

    Update Counter.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { MealsContext } from "./MealsProvider";
 
 const Counter = () => {
+  const { meals } = useContext(MealsContext);
+  const remainingMeals = meals.filter((meal) => !meal.ticked).length;
 
-    return (
-        <div>
-            <h3>Meals Remaining: remaingMeals</h3>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h3>Meals Remaining: {remainingMeals}</h3>
+    </div>
+  );
+};
 
 export default Counter;
